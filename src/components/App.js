003ee99535f514f2b033c052f37dd5d929e794a7
Joseph Watkins <josephxwatkins@gmail.com>
2022-06-13 18:42:26 -0400

    connect a better premade action complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from 'react';
 import reducer, { initialState } from '../reducers/index';
-import { addOne }  from '../actions/index';
+// import { addOne }  from '../actions/index';
+import { applyNumber } from '../actions/index';
 
 import './App.css';
 
@@ -11,8 +12,12 @@ function App() {
 
   const [ state, dispatch ] = useReducer(reducer, initialState)
 
-  const handleAdd = () => {
-    dispatch(addOne())
+  // const handleAdd = () => {
+  //   dispatch(addOne())
+  // }
+
+  const handleNum = (number) => {
+    dispatch(applyNumber(number));
   }
 
   return (
@@ -39,21 +44,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton onClick={handleAdd} value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={() => handleNum(1)}/> 
+              <CalcButton value={2} onClick={() => handleNum(2)}/>
+              <CalcButton value={3} onClick={() => handleNum(3)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={() => handleNum(4)}/>
+              <CalcButton value={5} onClick={() => handleNum(5)}/>
+              <CalcButton value={6} onClick={() => handleNum(6)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={() => handleNum(7)}/>
+              <CalcButton value={8} onClick={() => handleNum(8)}/>
+              <CalcButton value={9} onClick={() => handleNum(9)}/>
             </div>
 
             <div className="row">
